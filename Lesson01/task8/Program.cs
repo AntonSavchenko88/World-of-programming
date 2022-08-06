2165422dcf539f8c4f76ea11b1adc9e766b4701b
«Anton» <asavchenko.1988@mail.ru>
2022-08-06 19:59:00 +0300

    Решена задача №8, добавленна блок схема задачи №8

--- Lesson01/task8/Program.cs
+++ Lesson01/task8/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+int a = int.Parse(Console.ReadLine());
+int b = 1;
+while (b<a)
+{
+    if (b % 2==0)
+    {
+     Console.WriteLine (b);
+  b = b+1;
+    }
+    else
+    {
+        b = b+1;
+    }
+}
