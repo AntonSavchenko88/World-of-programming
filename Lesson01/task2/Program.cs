af70c199ef8bf9939a8b1eb42d5850a3f0c28c8f
«Anton» <asavchenko.1988@mail.ru>
2022-08-06 17:51:20 +0300

    Изменена структура проекта, переписан код задачи №2

---
+++ Lesson01/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int a = 17; //new Random().Next(-10,20);
+Console.WriteLine(a);
+int b = 8; //new Random().Next(-10,20);
+Console.WriteLine(b);
+int max = a;
+int min = b;
+if (a > b)
+{
+max = a;
+Console.Write("max=");
+Console.WriteLine (a);
+Console.Write("min=");
+Console.Write (b);
+} 
+else
+{
+max = b;
+Console.Write("max=");
+Console.WriteLine (b);
+Console.Write("min="); 
+Console.Write (a);   
+}
