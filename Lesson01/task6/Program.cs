d593963b11e2e5ac65802762e120d418a6cb15fd
«Anton» <asavchenko.1988@mail.ru>
2022-08-06 19:00:51 +0300

    Решена задача №6, добавленна блок схема задачи №6

--- Lesson01/task6/Program.cs
+++ Lesson01/task6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+int a = int.Parse(Console.ReadLine());
+if (a % 2==0)
+{
+Console.WriteLine("Число четное");
+}
+else
+{
+Console.WriteLine("Число не четное");
+}
