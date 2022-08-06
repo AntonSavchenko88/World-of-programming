4a7e1c7ab12ec692e6425bcfbbb2bc7727a6b1ca
«Anton» <asavchenko.1988@mail.ru>
2022-08-06 18:29:13 +0300

    Решена задача №4, добавленнв блок схема по задаче №4

--- Lesson01/task4/Program.cs
+++ Lesson01/task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+int a = 2;
+int b = 5;
+int c = 7;
+int max = a;
+while (b > max)
+{
+    max = b;
+}
+while (c>max)
+{
+    max=c;
+}
+Console.WriteLine(max);
