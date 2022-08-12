e3d4e0c486319e737ee1bebe07e7e1891fe3cabb
«Anton» <asavchenko.1988@mail.ru>
2022-08-13 00:31:14 +0300

    Решена задача № 15

--- Lesson02/task15/Program.cs
+++ Lesson02/task15/Program.cs
@@ -3,3 +3,18 @@
 6 -> да
 7 -> да
 1 -> нет*/
+
+Console.WriteLine("Введите цифру дня недели - ");
+int day= int.Parse (Console.ReadLine());
+if (day>7)
+{
+    Console.WriteLine("Ошибка, в неделе всего 7 дней!");
+}
+else  if (day==6||day==7)
+    {
+        Console.WriteLine("Поздравляю, это выходной день!");
+    }
+    else
+    {
+        Console.WriteLine("Это рабочий день!");
+    }
