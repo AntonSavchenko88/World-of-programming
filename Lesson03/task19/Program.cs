894da50c683ae7011a2e95e09e07856e2863c2a9
«Anton» <asavchenko.1988@mail.ru>
2022-08-16 23:09:13 +0300

    Решены задачи 3-го семинара №№19,21,23.

---
+++ Lesson03/task19/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+/*Console.WriteLine("Введите пятизначное число- ");// Этот пример был разобран на семинаре.
+int a= int.Parse (Console.ReadLine());
+int t=a;
+int na=0;
+while (a!=0)
+{
+    na= na * 10 + a % 10;
+    a/=10;
+}
+Console.WriteLine ($"a==na: {t==na}");*/
+Console.WriteLine("Введите пятизначное число- ");
+int a=int.Parse(Console.ReadLine());
+string ar=a.ToString();
+char [] array = ar.ToCharArray();
+if (array.Length==5)
+{
+    if (array[0]==array[4]&&array[1]==array[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+else 
+{
+    Console.WriteLine("Введите пятизначное число");
+}
---
+++ Lesson03/task21/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+Console.WriteLine("Введите кординату X- точки А");
+double X= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите кординату Y- точки А");
+double Y= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите кординату Z- точки А");
+double Z= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите кординату X- точки B");
+double Xx=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите кординату Y- точки B");
+double Yy=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите кординату Z- точки B");
+double Zz=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Координаты точки А="+X+","+Y+","+Z+".");
+Console.WriteLine("Координаты точки B="+Xx+","+Yy+","+Zz+".");
+
+double AB=Math.Sqrt(Math.Pow((Xx-X),2) + Math.Pow((Yy-Y),2)+Math.Pow((Zz-Z),2));
+Console.WriteLine("Расстояние между точками А и В="+Math.Round(AB,2));
