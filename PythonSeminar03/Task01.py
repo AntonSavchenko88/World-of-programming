eed7f8122392b80e5005c97f9f81bec7ba1825b2
«Anton» <asavchenko.1988@mail.ru>
2022-12-03 21:33:03 +0300

    Решена задача №2

--- PythonSeminar03/Task01.py
+++ PythonSeminar03/Task01.py
@@ -7,10 +7,10 @@ import random
 list_size = int(input("Введите размерность списка который хотите создать - "))
 list = []
 for i in range(list_size):
-    list.append(random.randint(0,10))
+    list.append(random.randint(0, 10))
 print(list)
 sum_elements = 0
 for i in range(len(list)):
     if i % 2 == 1:
-        sum_elements += list[i]       
+        sum_elements += list[i]
 print(f"{list} Cумма элементов на нечётных позициях = {sum_elements}")
--- PythonSeminar03/Task02.py
+++ PythonSeminar03/Task02.py
@@ -4,3 +4,17 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+import random
+list_size = int(input("Введите размерность списка который хотите создать - "))
+list = []
+newlist=[]
+for i in range(list_size):
+    list.append(random.randint(0, 10))
+print(list)
+for i in range(len(list)):
+    while i < len(list)/2 and list_size > len(list)/2:
+        list_size = list_size - 1
+        a = list[i] * list[list_size]
+        newlist.append(a)
+        i += 1
+print(f"Произведение пар чисел в списке = {newlist}")
