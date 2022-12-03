61423e147a1f8499c331147d6fbc4551228f30e3
«Anton» <asavchenko.1988@mail.ru>
2022-12-04 00:09:15 +0300

    Решена задача №5

--- PythonSeminar03/Task05.py
+++ PythonSeminar03/Task05.py
@@ -3,3 +3,28 @@
 # Пример:
 
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+def Fib(n):
+    if n in [1, 2]:                       
+        return 1
+    else:
+        return Fib(n-1) + Fib(n-2)
+# Дальше решение пришлось подглядывать(((
+def NotFib(n):
+    if n == 1:                       
+        return 1
+    elif n == 2:                       
+        return -1
+    else:
+        num1, num2 = 1, -1
+        for i in range(2, n):
+            num1, num2 = num2, num1 - num2
+        return num2
+
+list = [0]
+num = int(input('Enter any number: '))
+for i in range(1, num + 1):
+    list.append(Fib(i))
+    list.insert(0, NotFib(i))
+print(list)
