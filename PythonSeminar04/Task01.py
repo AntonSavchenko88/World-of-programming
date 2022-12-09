e9975954cbf2aec796511d964de1e167c0c1279d
«Anton» <asavchenko.1988@mail.ru>
2022-12-09 21:00:31 +0300

    Решена задача №1

--- PythonSeminar04/Task01.py
+++ PythonSeminar04/Task01.py
@@ -1,19 +1,13 @@
 # Вычислить число c заданной точностью d
-
 # Пример:
-
 # - при d = 0.001, π = 3.141
 # Ввод: 0.01
 #     Вывод: 3.14
-
 #     Ввод: 0.001
 #     Вывод: 3.141
 import math
-print(math.pi)
 accuracy = float(input("Введите требуемую точность округления числа Пи: "))
-print(accuracy)
-if accuracy >=0.1:
+if accuracy >= 0.1:
     print("Введено ошибочное значение, пожалуйста повторите ввод!")
-#print(len(str(accuracy).split('.')))
-num=len(str(accuracy).split('.'))
-print(num)
+num = int(len(str(accuracy).split('.')[1]))
+print("Округленное число= ",round(math.pi, num))
